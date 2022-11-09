ac5219211754f629648913ff602834a558f49ce0
RHN007 <rakibul.nayon@outlook.com>
2022-11-09 11:34:11 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 9000;
 require('dotenv').config()
 
 app.use(cors());
 app.use(express.json())
+
+
+
+// Mongodb Connection 
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4nsbrbj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        const serviceCollection = client.db('healthCoach').collection('services')
+
+        //To find all the data from servers 
+    app.get('/services', async(req, res)=> {
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services)
+      })
+
+
+
+    }
+    catch{
+
+    }
+}
+run().catch(err => console.error(err))
+
+
+
+
 
 
 app.get('/', (req, res)=> {
