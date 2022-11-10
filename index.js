60dd34912ff104104d727ef904068b3fbbbadf43
RHN007 <rakibul.nayon@outlook.com>
2022-11-10 11:18:50 +0600

    loading services

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 9000;
 require('dotenv').config()
 
@@ -23,12 +23,25 @@ async function run () {
 
         //To find all the data from servers 
     app.get('/services', async(req, res)=> {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+          })
+
+    app.get('/services/limit', async(req, res)=> {
         const query = {}
         const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
+        const services = await cursor.limit(3).toArray();
         res.send(services)
       })
 
+    app.get('/services/:id', async(req, res)=> {
+        const id = req.params.id; 
+        const query = {_id:ObjectId(id)}
+        const service = await serviceCollection.findOne(query)
+        res.send(service)
+    })
 
 
     }
